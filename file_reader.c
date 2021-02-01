36c9625e3a2381a60e9a747a5bfab3f017a2f545
Rudransh Dixit <dixitrudransh01@gmail.com>
2021-02-01 16:05:57 +0530

    added some functions and structs to make storing data easy

--- file_reader.c
+++ file_reader.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 
-int init(int argc, char const *argv[])
+struct header
 {
-    char n[3];
-    n[3] = 0;
-    int header[3];
+    char id[2];
+    int file_size, reserved, data_offset;
+} __attribute__((packed));
+
+struct info_header
+{
+    int size, width, height;
+    short planes, bits_per_pixel;
+    int compression, image_size, x_pp_m, y_pp_m, colors_used, important_colors;
+};
+
+struct pixel_data
+{
+    char red, blue, green;
+};
+
+void logh(struct header *h);
+void logih(struct info_header *ih);
+
+int init()
+{
+    struct header h;
+    struct info_header ih;
     FILE *bmp_24 = NULL;
 
     bmp_24 = fopen("bmp_24.bmp", "rb");
-    fread(&n, 2, 1, bmp_24);
-    fread(&header, sizeof(header), 1, bmp_24);
-    printf("Header:");
-    printf("%s\n", n);
-    for (int i = 0; i < 3; i++)
-    {
-        printf("%d\n", header[i]);
-    }
-
+    printf("%lu\n", sizeof(h));
+    fread(&h, sizeof(h), 1, bmp_24);
+    fread(&ih, sizeof(struct info_header), 1, bmp_24);
+    logh(&h);
+    logih(&ih);
     return 0;
+}
+
+void logh(struct header *h)
+{
+    printf("%c%c %d %d %d\n", (*h).id[0], (*h).id[1], (*h).file_size, (*h).reserved, (*h).data_offset);
+}
+
+void logih(struct info_header *ih)
+{
+    printf("%d %d %d %d %d %d %d %d %d %d %d\n", (*ih).size, (*ih).width, (*ih).height, (*ih).planes, (*ih).bits_per_pixel, (*ih).compression, (*ih).image_size, (*ih).x_pp_m, (*ih).y_pp_m, (*ih).colors_used, (*ih).important_colors);
 }
