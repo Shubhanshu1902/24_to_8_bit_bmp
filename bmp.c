ae7ee67128f873f4769ce67486220bab29e3a5e3
Shubhanshu1902 <shubhanshu1902@gmail.com>
2021-02-14 15:51:05 +0530

    Co-authored-by: IMT2020109 Chinthan Chandra <Chinthan.Chandra@iiitb.org>

--- bmp.c
+++ bmp.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "bmp.h"
 struct BIT_MapHeader
 {
-	char name[2];                  //Name 
-	int size;
+    char name[2]; //Name
+    int size;
     int garbage;
-	unsigned int offset;    
-}__attribute__((packed));
+    unsigned int offset;
+} __attribute__((packed));
 
 struct DIB_Header
 {
-	unsigned int header_size;        //size of the header
-    unsigned int width;              //width of image 
-    unsigned int height;             //height of image
+    unsigned int header_size; //size of the header
+    unsigned int width;       //width of image
+    unsigned int height;      //height of image
     unsigned short int color_planer;
     unsigned short int bitsperpixel;
     unsigned int compression;
@@ -22,40 +22,78 @@ struct DIB_Header
     unsigned int vertical_resolution;
     int no_of_colors;
     int no_of_imp_colors;
+} __attribute__((packed));
+
+struct pixelarray
+{
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
 };
+
+struct image
+{
+    int height;
+    int width;
+    struct pixelarray **rgb;
+};
+
+struct image readimage(FILE *fp, int height, int width)
+{
+    struct image pic;
+    pic.rgb = (struct pixelarray**)malloc(height * sizeof(void *));
+    pic.height = height;
+    pic.width = width;
+    for (int i = height - 1; i>= 0; i--)
+    {
+        pic.rgb[i] = (struct pixelarray*)malloc(width * sizeof(struct pixelarray));
+        fread(pic.rgb[i],width,sizeof(struct pixelarray),fp);
+    }
+}
+
+void freedata(struct image pic)
+{
+    for (int i = pic.height - 1; i= 0; i--)
+    {
+        free(pic.rgb[i]);
+    }
+    free(pic.rgb);
+}
 
 struct BIT_MapHeader bitmapheader(FILE *fp)
 {
-	struct BIT_MapHeader header;
-	fread(&header,sizeof(struct BIT_MapHeader),1,fp);
-	return header;
+    struct BIT_MapHeader header;
+    fread(&header, sizeof(struct BIT_MapHeader), 1, fp);
+    return header;
 }
 
 struct DIB_Header dibheader(FILE *fp)
 {
-	struct DIB_Header dib_header;
-	fread(&dib_header,sizeof(struct DIB_Header),1,fp);
- 	return dib_header;  
+    struct DIB_Header dib_header;
+    fread(&dib_header, sizeof(struct DIB_Header), 1, fp);
+    return dib_header;
 }
 
-void open()
+void read()
 {
-	FILE *fp=fopen("lena_color.bmp","rb");
-	struct BIT_MapHeader header=bitmapheader(fp);
-    printf("%ld\n",sizeof(struct BIT_MapHeader));
-	printf("First two character:%c%c\n",header.name[0],header.name[1]);
-    printf("Size:%u\n",header.size);
-    printf("Image offset:%u\n",header.offset);
-    struct DIB_Header dib_header=dibheader(fp);
-    printf("Header size:%d\nwhidth:%d\nheight:%d\ncolor_planer:%d\nbitsperpixel:%d\ncompression:%d\nimage_size:%d\nhorizontal_resolution:%d\nvertical_resolution:%d\nno_of_colors:-%d\nno_of_imp_colors:%d\n",dib_header.header_size,
-    	dib_header.width,dib_header.height,dib_header.color_planer,
-    	dib_header.bitsperpixel,dib_header.compression,dib_header.image_size,dib_header.horizontal_resolution,
-    	dib_header.vertical_resolution,dib_header.no_of_colors,dib_header.no_of_imp_colors);
+    FILE *fp = fopen("lena_color.bmp", "rb");
+    struct BIT_MapHeader header = bitmapheader(fp);
+    //printf("%ld\n", sizeof(struct BIT_MapHeader));
+    //printf("First two character:%c%c\n", header.name[0], header.name[1]);
+    //printf("Size:%u\n", header.size);
+    //printf("Image offset:%u\n", header.offset);
+    struct DIB_Header dib_header = dibheader(fp);
+    /*printf("Header size:%d\nwhidth:%d\nheight:%d\ncolor_planer:%d\nbitsperpixel:%d\ncompression:%d\nimage_size:%d\nhorizontal_resolution:%d\nvertical_resolution:%d\nno_of_colors:-%d\nno_of_imp_colors:%d\n", dib_header.header_size,
+           dib_header.width, dib_header.height, dib_header.color_planer,
+           dib_header.bitsperpixel, dib_header.compression, dib_header.image_size, dib_header.horizontal_resolution,
+           dib_header.vertical_resolution, dib_header.no_of_colors, dib_header.no_of_imp_colors);*/
+    struct image pic=readimage(fp,dib_header.height,dib_header.width);
+    fclose(fp);
+    freedata(pic);
+
 }
 
 int main()
 {
-    open();
+    read();
 }
-
-
